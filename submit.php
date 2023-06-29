241255a904cfab8d9796ab68254f06c521c6698f
MaverickVP <106632579+MaverickVP@users.noreply.github.com>
2023-06-29 17:28:43 -0400

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,8 +1,14 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
   $comment = $_POST["comment"];
+  $email = $_POST["email"];
 
   // Validate the form data
   if (strlen($comment) < 25) {
@@ -10,18 +16,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     return;
   }
 
-  // Send the email
-  $to = "rebelliousanalytics@gmail.com"; // Replace with your email address
-  $subject = "New Comment";
-  $message = "Name: " . $name . "\n";
-  $message .= "Email: " . $email . "\n";
-  $message .= "Comment: " . $comment . "\n";
-  $headers = "From: " . $email;
+  // Send the email using PHPMailer
+  try {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->Username = 'rebelliousanalytics@gmail.com'; // Replace with your Gmail email address
+    $mail->Password = 'wmvlnygwoizcxcpz'; // Replace with your Gmail app password
+    $mail->setFrom($email);
+    $mail->addAddress('rebelliousanalytics@gmail.com'); // Replace with the recipient email address
+    $mail->Subject = 'New Comment';
+    $mail->Body = "Comment: " . $comment . "\n\nEmail: " . $email;
 
-  if (mail($to, $subject, $message, $headers)) {
+    $mail->send();
     echo "Comment submitted successfully.";
-  } else {
-    echo "Error sending the comment.";
+  } catch (Exception $e) {
+    echo "Error sending the comment. Error Info: " . $mail->ErrorInfo;
   }
 }
 ?>
